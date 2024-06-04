31a5b627fa6e712da7b8076abc2b23ed6862d95f
Aneesh-aj <rajuaneesh.p2020@gmail.com>
2024-06-04 09:06:33 +0530

    add changes

--- src/framework/service/socketIo.ts
+++ src/framework/service/socketIo.ts
@@ -9,25 +9,32 @@ import { userController } from '../webServer/routes/injections/Injection';
 
 
 export const server = http.createServer(app);
-export const io = new Server(server,{cors:{origin:"*"}});
+export const io = new Server(server, { cors: { origin: "*" } });
 
 
 io.on('connection', (socket) => {
     console.log('A user connected');
 
-  socket.on('selectSeat', (data: any) => {
+    socket.on('selectSeat', (data: any) => {
         console.log("caLLL===--------")
-        io.emit("seatSelected",{status:true})
+        io.emit("seatSelected", { status: true })
     });
 
-   
-   socket.on("sendData",(data)=>{
-    console.log(data,"-----------")
-      
-       
-    io.emit("resiveData",data)
-   })
-    
+
+
+    socket.on('joinRoom', ({ senderId, receiverId }) => {
+        const roomName = [senderId, receiverId].sort().join('-');
+        socket.join(roomName);
+        console.log(`User joined room: ${roomName}`);
+    });
+
+    socket.on("sendData", (data) => {
+        const { senderId, receiverId, message } = data;
+        const roomName = [senderId, receiverId].sort().join('-');
+        io.to(roomName).emit("resiveData", data);
+    });
+
+
     socket.on('disconnect', () => {
         console.log('User disconnected');
     });
