a53c865dc180cae903136daff8568209d6b85148
Aneesh-aj <rajuaneesh.p2020@gmail.com>
2024-06-14 20:09:56 +0530

    changes

--- src/framework/webServer/middlewares/auth.ts
+++ src/framework/webServer/middlewares/auth.ts
@@ -8,6 +8,9 @@ export const isAuthenticate = async (req: Request, res: Response, next: NextFunc
         const accessToken = req.cookies.accessToken as string;
         const refreshToken = req.cookies.refreshToken as string;
           console.log(" coming here-----------------req.cookiee----auth",req.cookies)
+          console.log("accessToken--",accessToken)
+          console.log("refreshToken---",refreshToken)
+          console.log(" user :---",req.cookies.role)
         if (!accessToken && !refreshToken) {
             return res.status(401).json({ message: "Access Forbidden!!! Please login again.", success: false });
         }
